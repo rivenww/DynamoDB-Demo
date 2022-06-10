0b9a6214461083e867ff308b6949bdd7a0aa9c8d
Riven <ruiwenwang1997@outlook.com>
2022-06-10 22:11:42 +0800

    Initial commit

---
+++ src/test/java/org/rivenstudio/dynamodemo/DynamoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rivenstudio.dynamodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
